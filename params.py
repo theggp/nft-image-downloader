e13270f9b565e178975df702f97ef76aaee7a9af
theggp <giovanni.pilon91@gmail.com>
2022-05-15 16:55:03 +0200

    files clean up

--- params.py
+++ params.py
@@ -1,33 +1,11 @@
-# # doodles
-project_name = 'doodles'
-project_contract = '0x8a90cab2b38dba80c64b7734e58ee1db38b8992e'
-n = 10000
-
-# # moonbirds
-# project_name = 'moonbirds'
-# project_contract = '0x23581767a106ae21c074b2276d25e5c3e136a68b'
-# n = 10000
-
-# # azuki
-# project_name = 'azuki'
-# project_contract = '0xed5af388653567af2f388e6224dc7c4b3241c544'
-# n = 10000
-
-# # boredapeyachtclub
-# project_name = 'boredapeyachtclub'
-# project_contract = '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'
-# n = 10000
-
 # # coolcats
-# project_name = 'coolcats'
-# project_contract = '0x1a92f7381b9f03921564a437210bb9396471050c'
-# n = 9999
-
-
+project_name = 'coolcats'
+project_contract = '0x1a92f7381b9f03921564a437210bb9396471050c'
+n = 9999
 
 # Max concurrent image requests
-n_workers = 10
+n_workers = 5
 
 # set batch_size = 500 to test the script on 500 images
 # set batch_size = n to download all
-batch_size = n
+batch_size = 500
